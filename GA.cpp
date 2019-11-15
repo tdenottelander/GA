5f3143605ca4067b8b5aa19710df42334f584d3b
Tom den Ottelander <tdenottelander@gmail.com>
2019-11-15 12:00:26 +0100

    Proportionate Selection

--- GA.cpp
+++ GA.cpp
@@ -20,7 +20,7 @@ auto rng = default_random_engine(0);
 class Individual {
     public:
         uvec genotype;
-        int fitness;
+        int fitness = -1;
         
         Individual(int length){
             genotype = uvec (length);
@@ -44,7 +44,11 @@ class Individual {
 
 class FitnessFunction {
     public:
-        Individual bestIndividual;
+        Individual bestIndividual = (0);
+
+        FitnessFunction(){
+            cout << "FitnessFunction created" << endl;
+        }
 
         int evaluate(Individual &ind){
             int result = 0;
@@ -63,6 +67,48 @@ class Selection {
             cout << "Do a selection" << endl;
         }
 };
+
+class ProportionateSelection : public Selection {
+    public:
+        vector<Individual> select(vector<Individual> &population, int outputSize) {
+            vector<Individual> selection;
+            selection.reserve(outputSize);
+            vector<float> proportions = getProportions(population);
+            for(int i = 0; i < outputSize; i++){
+                float randomValue = ((float)rand() / RAND_MAX);
+                for (int j = 0; j < proportions.size(); j++){
+                    if(randomValue < proportions[j]){
+                        selection.push_back(population[i]);
+                        break;
+                    }
+                }
+            }
+            return selection;
+        }
+
+        vector<float> getProportions(vector<Individual> &population){
+            int n = population.size();
+            vector<float> proportions (n);
+            int cumulativeFitness = 0;
+
+            for(Individual ind: population){
+                cumulativeFitness += ind.fitness;
+            }
+
+            avgFitness = (float)cumulativeFitness / n;
+
+            for(int i = 0; i < n; i++){
+                if(i == 0) {
+                    proportions[i] = (float)population[i].fitness / cumulativeFitness;
+                } else {
+                    proportions[i] = proportions[i-1] + (float)population[i].fitness / cumulativeFitness;
+                }
+            }
+
+            return proportions;
+        }
+};
+
 class Variation {
     public:
         void variate(){
@@ -101,6 +147,14 @@ class GAClass {
         void round(){
             selection.select();
             variation.variate();
+        }
+
+        double getAvgFitness(){
+            double result = 0;
+            for(Individual ind: population){
+                result += ind.fitness;
+            }
+            return result / population.size();
         }
 
         string toString() {
@@ -380,20 +434,10 @@ int main()
 {
     // GA();
 
-    // mat A = ones<mat>(4,5);
-    // mat B = randu<mat>(4,5);
-
-    // cout << A*B.t() << endl;
-
-    // uvec x(10);
-    // x.fill(0);
-
-    // vector<uvec> x = initPopulation(5, 10);
-
-    // for(uvec y: x) cout << y << endl;
-
     FitnessFunction func;
-    GAClass ga (5, 5, func);
+    Selection s;
+    Variation v;
+    GAClass ga (5, 5, func, s, v);
     cout << ga.toString() << endl;
 
     return 0;
