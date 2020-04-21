1d2a1adfa5d58637ca36a7a44834b4ebafd63599
Tom den Ottelander <tdenottelander@gmail.com>
2020-04-21 11:52:43 +0200

    Remove python scripts, imgs, NK landscapes, etc

--- mountAllMeteors.sh
+++
@@ -1,4 +0,0 @@
-sshfs meteor02:/export/scratch1/home/shared ~/mount/point2 -ovolname=meteor02_shared
-sshfs meteor03:/export/scratch1/home/shared ~/mount/point3 -ovolname=meteor03_shared
-sshfs meteor04:/export/scratch1/home/shared ~/mount/point4 -ovolname=meteor04_shared
-sshfs meteor05:/export/scratch1/home/shared ~/mount/point5 -ovolname=meteor05_shared
